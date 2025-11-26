ad12547b09f3c8600dbb9ffbd035effaa67d7955
joshheraldA <joshherald19@gmail.com>
2025-11-26 09:09:15 +0800

    Added James

--- LE6_2.java
+++ LE6_2.java
@@ -1,7 +1,7 @@
 class LE6_2 {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD");
-        System.out.println("NIGGA");
+        System.out.println("NIGGA JAMES");
 
     }
 }
